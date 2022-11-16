a89da255ebbd6db03b03830deeb0620b53de1ba5
lufarius <infinitevolume@gmail.com>
2022-11-16 14:15:46 -0800

    Fixed issue that was limiting footer being responsive
    added a container to footer to expand background color
    used display flex on footer body
    removed padding for x axis for container media query @37.5em

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,42 +4,44 @@ import footer from "./Footer.module.css";
 
 export default function Footer() {
     return (
-        <div className={footer.body}>
-            <Image className={footer.logo} src="/images/logo-light.svg" alt="Light Logo" width={60} height={32}></Image>
-            <ul className={footer.list}>
-                <li>
-                    <Link href="/" className={footer.link}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/about" className={footer.link}>
-                        Portfolio
-                    </Link>
-                </li>
-                <li>
-                    <Link href="/contact" className={footer.link}>
-                        Contact Me
-                    </Link>
-                </li>
-            </ul>
-            <ul className={footer.social}>
-                <li>
-                    <Link href="#">
-                        <Image src="/images/icons/github-light.svg" alt="github logo and link" width={25} height={24}></Image>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="#">
-                        <Image src="/images/icons/twitter-light.svg" alt="twitter logo and link" width={24} height={19.5}></Image>
-                    </Link>
-                </li>
-                <li>
-                    <Link href="#">
-                        <Image src="/images/icons/linkedin-light.svg" alt="Linkedin logo and link" width={24} height={24}></Image>
-                    </Link>
-                </li>
-            </ul>
+        <div className={footer.container}>
+            <div className={footer.body}>
+                <Image className={footer.logo} src="/images/logo-light.svg" alt="Light Logo" width={60} height={32}></Image>
+                <ul className={footer.list}>
+                    <li>
+                        <Link href="/" className={footer.link}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/about" className={footer.link}>
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" className={footer.link}>
+                            Contact Me
+                        </Link>
+                    </li>
+                </ul>
+                <ul className={footer.social}>
+                    <li>
+                        <Link href="#">
+                            <Image src="/images/icons/github-light.svg" alt="github logo and link" width={25} height={24}></Image>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <Image src="/images/icons/twitter-light.svg" alt="twitter logo and link" width={24} height={19.5}></Image>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="#">
+                            <Image src="/images/icons/linkedin-light.svg" alt="Linkedin logo and link" width={24} height={24}></Image>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
