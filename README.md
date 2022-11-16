02fd92279a3240b461f7bd269859b991d96e40ac
lufarius <infinitevolume@gmail.com>
2022-11-15 18:48:32 -0800

    Setup Header for mobile view with hamburger menu

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 https://nextjs.org/docs/api-reference/next/font
+
+//hamburger menu
+https://justmarkup.com/articles/2019-12-04-hamburger-menu/
--- components/header.tsx
+++ components/header.tsx
@@ -1,32 +1,64 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.css";
+import { useState } from "react";
 
 export default function Header() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    const handleToggle = () => {
+        setNavbarOpen((prev) => !prev);
+    };
     return (
         <div className={styles.container}>
-            <header className={styles.header}>
-                <Image src="/images/logo.svg" alt="Arrow Logo" width={60} height={32}></Image>
-                <nav className={styles.nav}>
+            <div>
+                <header className={styles.header}>
+                    <Image className={styles.img} src="/images/logo.svg" alt="Arrow Logo" width={60} height={32}></Image>
+                    {/* <nav className={styles.nav}>
                     <ul className={styles.list}>
                         <li className={styles.item}>
-                            <Link href="/" className={styles.link}>
+                        <Link href="/" className={styles.link}>
+                        Home
+                        </Link>
+                        </li>
+                        <li className={styles.item}>
+                        <Link href="/about" className={styles.link}>
+                                Portfolio
+                            </Link>
+                            </li>
+                            <li className={styles.item}>
+                            <Link href="/contact" className={styles.link}>
+                            Contact Me
+                            </Link>
+                            </li>
+                            </ul>
+                </nav> */}
+                    <button className={`${styles.menu} ${navbarOpen ? "close" : "open"}`} aria-controls="menu" onClick={handleToggle}>
+                        {navbarOpen ? (
+                            <Image src="/images/icons/close.svg" alt="close nav menu" width={18} height={19}></Image>
+                        ) : (
+                            <Image src="/images/icons/hamburger.svg" alt="open nav menu" width={24} height={13}></Image>
+                        )}
+                    </button>
+                    <ul className={`menuNav ${navbarOpen ? styles.showMenu : styles.hideMenu}`}>
+                        <li className={styles.item}>
+                            <Link href="/" className={styles.burgerItem}>
                                 Home
                             </Link>
                         </li>
                         <li className={styles.item}>
-                            <Link href="/about" className={styles.link}>
+                            <Link href="/about" className={styles.burgerItem}>
                                 Portfolio
                             </Link>
                         </li>
                         <li className={styles.item}>
-                            <Link href="/contact" className={styles.link}>
+                            <Link href="/contact" className={styles.burgerItem}>
                                 Contact Me
                             </Link>
                         </li>
                     </ul>
-                </nav>
-            </header>
+                </header>
+            </div>
         </div>
     );
 }
