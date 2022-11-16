ee2ce2eb673c526629b34ea3a97e0382e2788471
lufarius <infinitevolume@gmail.com>
2022-11-16 11:34:31 -0800

    Added styling to Footer for mobile

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,29 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "./Header.module.css";
 import footer from "./Footer.module.css";
+
 export default function Footer() {
     return (
         <div className={footer.body}>
-            <Image src="/images/logo-light.svg" alt="Light Logo" width={60} height={32}></Image>
+            <Image className={footer.logo} src="/images/logo-light.svg" alt="Light Logo" width={60} height={32}></Image>
             <ul className={footer.list}>
-                <li className={styles.item}>
+                <li>
                     <Link href="/" className={footer.link}>
                         Home
                     </Link>
                 </li>
-                <li className={styles.item}>
+                <li>
                     <Link href="/about" className={footer.link}>
                         Portfolio
                     </Link>
                 </li>
-                <li className={styles.item}>
+                <li>
                     <Link href="/contact" className={footer.link}>
                         Contact Me
                     </Link>
                 </li>
             </ul>
-            <ul>
+            <ul className={footer.social}>
                 <li>
                     <Link href="#">
                         <Image src="/images/icons/github-light.svg" alt="github logo and link" width={25} height={24}></Image>
