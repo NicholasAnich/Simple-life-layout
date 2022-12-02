529af28fe3274aada45bdd58298da2304421132a
lufarius <infinitevolume@gmail.com>
2022-12-01 19:07:23 -0800

    Added Portfolio Reponsive Design for Tabelet

--- components/designs.tsx
+++ components/designs.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
+import { useState } from "react";
 import portfolio from "../styles/Portfolio.module.css";
 import designs from "./Designs.module.css";
 
 export default function Designs({ id, ...project }) {
+    // const [isReversed, setIsReversed] = useState(false);
     const {
         projectName,
         summary,
         images: { mobile, tablet, desktop },
     } = project;
-    console.log({ mobile, tablet, desktop });
+    // console.log({ mobile, tablet, desktop });
+    let isReversed = false;
+
+    if (id % 2 !== 0) {
+        isReversed = true;
+    }
+
     return (
-        <div key={`${projectName}${id}`}>
-            <picture>
-                <source srcSet={`${desktop.med} 540w, ${desktop.lg} 1080w`} media="(min-width: 960px)" />
-                <source srcSet={`${tablet.med} 339w, ${tablet.lg} 678w`} media="(min-width: 600px)" />
-                <source srcSet={`${mobile.sm}, ${mobile.med} 622w`} />
-                <img className={designs.images} src={mobile.sm} srcSet={`${mobile.med} w622`} alt={projectName} />
-            </picture>
+        <div className={`${designs.container} ${isReversed ? designs.reversed : null}`} key={`${projectName}${id}`}>
+            <div className={designs.imageContainer}>
+                <picture>
+                    <source srcSet={`${desktop.med} 540w, ${desktop.lg} 1080w`} media="(min-width: 960px)" />
+                    <source srcSet={`${tablet.med} 339w, ${tablet.lg} 678w`} media="(min-width: 600px)" />
+                    <source srcSet={`${mobile.sm} 311w, ${mobile.med} 622w`} />
+                    <img className={designs.images} src={mobile.sm} srcSet={`${mobile.med} w622`} alt={projectName} />
+                </picture>
+            </div>
             <div className={designs.body}>
                 <h2 className={designs.title}>{projectName}</h2>
                 <p className={designs.summary}>{summary}</p>
